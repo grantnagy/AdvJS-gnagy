93f7c6d205eb201096656825fdf9af79c4fca643
Grant <grantnagy11@gmail.com>
2020-02-04 12:24:38 -0700

    start, based off different.js

--- kattis/above average/aa.js
+++ kattis/above average/aa.js
@@ -6,40 +6,50 @@ const readline = require('readline');
 
 function answer(line)
 {
-    let nums = parseInt(line.split(" "));
+    let nums = line.split(" ");
     let ans = 0;
     let average = 0;
 
-    for (let grade of line)
+    for (var i in nums)
     {
-        if (grade => 80) average++;
-        
+        if (parseInt(nums[i]) >= 80) average++;
     }
-    average = average / line[0];
-
+    console.log(average);
+    console.log(parseInt(nums[0]));
+    average = average / parseInt(nums[0]);
+    ans = 100* average.toPrecision(5) + '%';
     return ans;
 }
 function test()
 {
-    assert.strictEqual(answer(100, 99), 1);
-    assert.strictEqual(answer(7200, 534), 6666);
-    assert.strictEqual(answer(2020, 82098), 80078);
+    assert.strictEqual(answer(5, 50, 50, 70, 80, 100), 40.000);
     console.log("all test cases passed!");
 }
 function kattis(){
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
-    })
-    let lineNum = 1;
-    rl.on("line", line => {
-        if (lineNum == 1) lineNum++;
-        else {
-            console.log(answer(line));
-        }
     });
+
+    let lineNum = 1;
+
+   /* rl.on("line", line => {
+        if (lineNum == 1) { 
+            let testcases = line.split(" ");
+            lineNum++; 
+        }
+        for (var i = 0; i <= parseInttestcases[0]; i++) console.log(answer(line));
+    });
+    */
+   rl.on("line", line => {
+    if (lineNum == 1) lineNum++; 
+    else {
+        console.log(answer(line));
+        rl.close();
+    }
+});
 }
-}
+
 
 if (require.main == module) {
     if (process.argv.length > 2 && process.argv[2] === 'test')
